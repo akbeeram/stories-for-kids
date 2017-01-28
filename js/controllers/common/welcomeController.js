932250c42ac3c7ae2a19864e5d30b7b37cb1f464
akbeeram <anilkumarb404@gmail.com>
2017-01-28 16:50:58 +0530

    added login call to all story tiles

--- js/controllers/common/welcomeController.js
+++ js/controllers/common/welcomeController.js
@@ -4,5 +4,14 @@
     angular.module('welcomeModule',['directiveModule'])
         .controller('welcomeController',function($scope){
             $scope.storyCategory=['A','B','C','D','E','F','G','H','I','J'];
+        
+            $scope.openLoginLightBox=function(){
+                //if user logged in take them to dashboard
+                //if user not logged in show login light box
+                $scope.showLoginForm = true;
+                $scope.$broadcast('showLoginFormEvent', {
+                  showLoginForm: true 
+                });
+            }
         });
 }(angular));
