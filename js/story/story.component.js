8936ea9fcbf800ee966d7f481339d36606c643d6
akbeeram <anilkumarb404@gmail.com>
2017-02-12 17:16:28 +0530

    header styling fixed

--- js/story/story.component.js
+++ js/story/story.component.js
@@ -12,7 +12,7 @@ storyComponentCtrl.$inject = ['$scope','$state','$timeout'];
 function storyComponentCtrl($scope, $state, $timeout){
     var vm = this;
     vm.getTemplateUrl = function(){
-        console.log(vm.config);
+        //console.log(vm.config);
         return 'js/stories/panchatantra/'+vm.config.story_html_name+'.html';
     }
 }
