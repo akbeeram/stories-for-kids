bb0eacb37a679fae7d85f881fdd471f7e6345b3b
akbeeram <anilkumarb404@gmail.com>
2017-02-18 08:37:20 +0530

    add 1st story for 2nd category

--- js/story/story.component.js
+++ js/story/story.component.js
@@ -11,8 +11,19 @@ angular.module('storiesApp')
 storyComponentCtrl.$inject = ['$scope','$state','$timeout'];
 function storyComponentCtrl($scope, $state, $timeout){
     var vm = this;
+    var templateUrl = '';
     vm.getTemplateUrl = function(){
         //console.log(vm.config);
-        return 'js/stories/panchatantra/'+vm.config.story_html_name+'.html';
+        if(vm.config && vm.config.story_cat_id){
+            switch (vm.config.story_cat_id) {
+                case 'CG001':
+                    templateUrl = 'js/stories/panchatantra/'+vm.config.story_html_name+'.html';
+                    break;
+                case 'CG005':
+                    templateUrl = 'js/stories/vikram-baethal/'+vm.config.story_html_name+'.html';
+                    break;
+            }
+        }
+        return templateUrl;
     }
 }
